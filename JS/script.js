e0f6707810983f27377329e32d02ca57509e2309
Novo <Novo@22IFMA0338.ifma.edu>
2025-08-30 11:03:39 -0300

    fodase

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+// console.log('Hello World')
+
+// alert("bem vindo")
+
+// pega o formulario de contato pelo id
+let form = document.getElementById("contact-form")
+// adicona um ouvinte de evento para o envio de formulario
+form.addEventListener("submit", enviarFormulario);
+// função que sera chamada quando o formulario for enviado
+function enviarFormulario(event){
+    event.preventDefault();
+    let nome = document.getElementById("name").value;
+    console.log(nome);
+    alert(nome+ "Seu form foi enviado")
+}
