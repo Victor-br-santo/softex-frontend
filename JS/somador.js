e3e909729c65e09c50a7292dfb7a2f5c2b5d643d
Novo <Novo@22IFMA0338.ifma.edu>
2025-09-20 10:10:45 -0300

    hfh

--- JS/somador.js
+++ JS/somador.js
@@ -11,23 +11,38 @@
 // }
 // console.log("a quantidade é de numeros multiplos é:", cont_1);
 
-let numero = 11;
-let contDivisores = 0;
-let divisor = 2;
+// let numero = 11;
+// let contDivisores = 0;
+// let divisor = 2;
 
-while(numero != 1){
- if(numero%divisor!=0){
-    divisor++;
-    console.log("Divisor testado: ", + divisor);
- }else{
-    console.log("Divisor encontrado: ", + divisor);
-    numero = numero / divisor;
-    contDivisores++;
- }
-}
-if(contDivisores < 2){
-     console.log("O numero é primo");
-}else{
-     console.log("O numero não é primo");
+// while (numero != 1) {
+//    if (numero % divisor != 0) {
+//       divisor++;
+//       console.log("Divisor testado: ", + divisor);
+//    } else {
+//       console.log("Divisor encontrado: ", + divisor);
+//       numero = numero / divisor;
+//       contDivisores++;
+//    }
+// }
+// if (contDivisores < 2) {
+//    console.log("O numero é primo");
+// } else {
+//    console.log("O numero não é primo");
+// }
+
+
+let botao = document.getElementById("calcular");
+botao.addEventListener("click", calcularTabuada);
+
+function calcularTabuada() {
+   let input = document.getElementById("numero").value;
+   let numero = parseInt(input);
+   // calculo da tabuada
+   let resultado ="";
+   for(let i = 1; i <= 10; i++){
+      resultado = numero+" x "+i+" = " +(numero*i)+"\n";
+      console.log(resultado)
+   }
 }
 
