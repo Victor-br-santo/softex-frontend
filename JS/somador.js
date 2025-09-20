8fa0d8b1e516e70723ec1a41aa315b0a8c50f3be
Novo <Novo@22IFMA0338.ifma.edu>
2025-09-19 21:12:27 -0300

    fuhaf

---
+++ JS/somador.js
@@ -0,0 +1,33 @@
+// let cont = 0;
+// let cont_1 = 0;
+// let media = 0;
+// let qtd = 20000;
+
+// while (cont <= qtd) {
+//     if (cont % 3 == 1) {
+//         cont_1 ++;
+//     }
+//     cont ++;
+// }
+// console.log("a quantidade é de numeros multiplos é:", cont_1);
+
+let numero = 11;
+let contDivisores = 0;
+let divisor = 2;
+
+while(numero != 1){
+ if(numero%divisor!=0){
+    divisor++;
+    console.log("Divisor testado: ", + divisor);
+ }else{
+    console.log("Divisor encontrado: ", + divisor);
+    numero = numero / divisor;
+    contDivisores++;
+ }
+}
+if(contDivisores < 2){
+     console.log("O numero é primo");
+}else{
+     console.log("O numero não é primo");
+}
+
